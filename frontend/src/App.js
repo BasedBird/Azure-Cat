fb2450ab30b1dd400d48d226c961f7c4570782c0
BasedBird <basedbird445@gmail.com>
2024-03-20 21:35:30 -0400

    Set up some routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import ImageForm from './components/ImageForm';
+import { CallbackPage } from './pages/callback-page';
+import { MainPage } from './pages/main-page';
+
 
 function App() {
   return (
     <div className="container">
-      <ImageForm/>
+      <Routes>
+        <Route path="/main" element = {<MainPage />} />
+        <Route path="/" element = {<CallbackPage />} />
+      </Routes>
     </div>
   );
 }
